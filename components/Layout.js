9b68a945c6e5ab28323795ef19fa66ffd4a51297
Alexandra Spalato <alexaspalato@gmail.com>
2021-01-08 16:45:13 +0100

    env

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,8 @@
 import { Header } from "./header"
 import { Footer } from "./footer"
 
-export const Layout = ({ children, layoutData, ...props }) => {
+export const Layout = ({ children, layoutData = {}, ...props }) => {
+  console.log("layoutData", layoutData)
   const { menusData, headerFooterData } = layoutData
   const { getFooter: footerData } = headerFooterData
   const headerData = { menusData, getHeader: headerFooterData.getHeader }
--- config.js
+++
@@ -1,4 +0,0 @@
-import slashes from "remove-trailing-slash"
-export const config = {
-  wpUrl: slashes(`http://localhost:10028/`),
-}
