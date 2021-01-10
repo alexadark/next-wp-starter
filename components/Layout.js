f0a87a8dd14ecac79b34cad4714aa4232c9e0cc0
Alexandra Spalato <alexaspalato@gmail.com>
2021-01-10 16:28:48 +0100

    layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,16 +1,47 @@
 import { Header } from "./header"
 import { Footer } from "./footer"
+import { Box, Flex, chakra } from "@chakra-ui/react"
+import Headroom from "react-headroom"
 
-export const Layout = ({ children, layoutData = {}, ...props }) => {
-  console.log("layoutData", layoutData)
+export const Layout = ({
+  children,
+  page = {},
+  layoutData = {},
+  type = "page",
+  ...props
+}) => {
   const { menusData, headerFooterData } = layoutData
   const { getFooter: footerData } = headerFooterData || {}
   const headerData = { menusData, getHeader: headerFooterData?.getHeader }
+
+  const layoutClass = page !== undefined ? (page.slug ? page.slug : page) : ""
+  const pageTemplate = page?.template
+    ? page?.template?.templateName?.toLowerCase()
+    : ""
+  const fullWidthClass = pageTemplate === "full width" ? "fullWidth" : ""
+
   return (
-    <div {...props}>
-      <Header headerData={headerData} />
-      {children}
+    <Flex
+      minHeight="100vh"
+      flexDirection="column"
+      className={`${layoutClass}=${type} ${fullWidthClass}`}
+      sx={{
+        "&.fullWidth": {
+          ".mainContainer": {
+            maxWidth: `100%`,
+            px: 0,
+          },
+        },
+      }}
+      {...props}
+    >
+      <chakra.Headroom sx={{ "&>div": { zIndex: "2!important" } }}>
+        <Header headerData={headerData} />
+      </chakra.Headroom>
+      <chakra.main flex={1} py={16}>
+        {children}
+      </chakra.main>
       <Footer footerData={footerData} />
-    </div>
+    </Flex>
   )
 }
