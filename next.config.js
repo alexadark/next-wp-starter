c4127b423fb1d4ee2d3df374a9f1ba69f95e8d94
Julián David <julianfullstackdev@gmail.com>
2021-04-06 21:16:49 -0500

    update packages

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
-const slashes = require("remove-trailing-slash")
-const imgHost = slashes(process.env.WP_URL).split("/")[2]
+const slashes = require("remove-trailing-slash");
+const imgHost = slashes(process.env.WP_URL).split("/")[2];
+
+console.log(imgHost, "esto que es");
 module.exports = {
   images: {
     domains: [imgHost],
   },
-}
+};
